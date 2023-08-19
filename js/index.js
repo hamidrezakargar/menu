c242850d34dae20b39fcfdbe0975c7c0cb3ba3bc
hamidreza <hamidrezakargar44@gmail.com>
2023-08-19 21:46:57 +0330

    filtr2

--- js/index.js
+++ js/index.js
@@ -6,3 +6,23 @@ new Splide('.splide', {
     pagination: false,
     direction: "rtl",
 }).mount();
+
+
+
+let list = document.querySelectorAll(".list li");
+let box = document.querySelectorAll(".box");
+
+list.forEach((el)=>{
+    el.addEventListener("click" , (e)=>{
+        list.forEach((el1)=>{
+            el1.style.color = "#000";
+        })
+        e.target.style.color = "#d4a373"
+        box.forEach((el2)=>{
+            el2.style.display = "none";
+        })
+        document.querySelectorAll(e.target.dataset.color).forEach((el3)=>{
+            el3.style.display = "flex";
+        })
+    })
+})
